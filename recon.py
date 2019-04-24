389292afd54bac71457e1f8d5063ab83563e2650
root <root@localhost.localdomain>
2019-04-24 12:01:36 +0530

    recon.py update with port scanning

--- recon.py
+++ recon.py
@@ -2,9 +2,29 @@ import requests
 from bs4 import BeautifulSoup as bs
 import optparse
 import socket
+import nmap
+def port_scanning(ip):
+    nmScan = nmap.PortScanner()
+    nmScan.scan(ip,arguments ='-A -sS -sV -vv -p 21-443')
+    for host in nmScan.all_hosts():
+        print "Host : %s (%s)" %(host, nmScan[host].hostname())
+        print "State : %s " % nmScan[host].state()
+        for proto in nmScan[host].all_protocols():
+            print '----------------------'
+            print 'Protocol : %s' %proto
+
+            lport = nmScan[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                print 'port :%s\tstate : %s' %(port, nmScan[host][proto][port]['state'])
+                print 'port :%s\tproduct : %s' %(port, nmScan[host][proto][port]['product'])
+                print 'port :%s\tname : %s' %(port, nmScan[host][proto][port]['name'])
+                print 'port :%s\tversion : %s' %(port, nmScan[host][proto][port]['version'])
 def host_ip(link_list):
     for url in link_list:
-        print url+'--->  '+str(socket.gethostbyname(url))
+        ip = socket.gethostbyname(url)
+        print url+'--->  '+str(ip)
+        result = port_scanning(ip)
 def extract_href(target_url):
     req = requests.get(target_url)
     html_soup = bs(req.text, 'html.parser')
